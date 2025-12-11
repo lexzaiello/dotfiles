90200d8a32f558da6d7ac10adbc819ff3d7e6ef5
Alexandra Aiello <dowlandaiello@gmail.com>
2025-12-11 11:29:05 -0800

    lockfile

--- flake.nix
+++ flake.nix
@@ -4,21 +4,30 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-colors.url = "github:misterio77/nix-colors";
+    mywm.url = "github:dowlandaiello/mywm";
+    proselint.url = "github:dowlandaiello/proselint.nix";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware }: {
+  outputs = { self, nixpkgs, ... }: {
     nixosModules.default = ./etc/nixos/hosts/default/configuration.nix;
     checks = {
-      check-config-build = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+      check-config-build = (nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit (self) inputs; # or just 'inherit inputs;' depending on scope
+        };
         modules = [
           self.nixosModules.default
-          nixos-hardware.nixosModules.framework-16-7040-amd
+          self.inputs.nixos-hardware.nixosModules.framework-16-7040-amd
           ({ config, pkgs, ... }: {
             nixpkgs.config.allowUnfree = true;
           })
         ];
-      };
+      }).config.system.build.toplevel;
     };
   };
 }
