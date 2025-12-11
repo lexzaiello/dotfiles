d47ad805b1a7613ff56241380e0da5e3d28295e4
Alexandra Aiello <dowlandaiello@gmail.com>
2025-12-11 10:36:01 -0800

    Nice tests

--- flake.nix
+++ flake.nix
@@ -3,9 +3,22 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, nixos-hardware }: {
     nixosModules.default = ./etc/nixos/hosts/default/configuration.nix;
+    checks = {
+      check-config-build = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          self.nixosModules.default
+          nixos-hardware.nixosModules.framework-16-7040-amd
+          ({ config, pkgs, ... }: {
+            nixpkgs.config.allowUnfree = true;
+          })
+        ];
+      };
+    };
   };
 }
