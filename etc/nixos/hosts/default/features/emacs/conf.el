416272c30abc83eda7a74a356b5634b5f7ceea8c
Alexandra Aiello <dowlandaiello@gmail.com>
2026-08-16 01:48:32 -0700

    spawn vterm buffer keybind wrong

--- etc/nixos/hosts/default/features/emacs/conf.el
+++ etc/nixos/hosts/default/features/emacs/conf.el
@@ -56,7 +56,7 @@
   (find-file my/org-home))
 
 (global-set-key (kbd "s-w") 'my/set-monitor)
-(global-set-key (kbd "s-RET") 'my/spawn-vterm-buffer)
+(global-set-key (kbd "s-<return>") 'my/spawn-vterm-buffer)
 (global-set-key (kbd "s-e") 'my/show-org-home)
 (global-set-key (kbd "C-S-SPC") 'my/launcher)
 
