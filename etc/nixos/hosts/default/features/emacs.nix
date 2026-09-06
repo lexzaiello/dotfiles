7effa828293dd2e316b8bfbe998ce40427f3a87b
Alexandra Aiello <dowlandaiello@gmail.com>
2026-09-06 00:21:33 -0700

    add agent-shell to emacs.nix

--- etc/nixos/hosts/default/features/emacs.nix
+++ etc/nixos/hosts/default/features/emacs.nix
@@ -69,6 +69,7 @@ in {
         ace-jump-mode
         ess
         agda2-mode
+        agent-shell
         (callPackage ./lean4-mode.nix {
           inherit (pkgs) fetchFromGitHub;
           inherit (pkgs.lib) fakeHash;
